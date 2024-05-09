e6c23cd8c4aee76cf55be519e51ff3882af86152
Jhon <bj856575@gmail.com>
2024-05-10 06:10:24 +0700

    Menambahkan beberapa operasi pada steram

--- src/test/java/belajar/java/stream/StreamOperationTest.java
+++ src/test/java/belajar/java/stream/StreamOperationTest.java
@@ -12,4 +12,25 @@ public class StreamOperationTest {
         Stream<String> stream = arr.stream();
         stream.forEach(s -> System.out.println(Integer.parseInt(s)));
     }
+    @Test
+    void testStreamPipelineOk(){
+        List<String> pipa = List.of("Naufal", "Abdul", "Basth");
+        pipa.stream()
+                .map(replace -> replace.replaceAll("[Aa]", "i"))
+                .map(String::toUpperCase)
+                .map(mister -> "Mr " + mister)
+                .forEach(System.out::println);
+    }
+    @Test
+    void testFilteringOpertation(){
+        List.of("Naufal", "Abdul", "Basth", "onah","ono","rizky").stream().filter(with -> with.length() <= 4).forEach(System.out::println);
+    }
+    @Test
+    void testFlatMapOpertations(){
+        List.of("Naufal", "Abdul", "Basth", "onah","ono","rizky").stream()
+                .flatMap(mapper -> Stream.of(mapper.length(), mapper.toLowerCase()))
+                .flatMap(s -> Stream.of(s,s,s,s))
+                .distinct()
+                .forEach(System.out::println);
+    }
 }
